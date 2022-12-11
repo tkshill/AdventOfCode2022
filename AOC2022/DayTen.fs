eb56c811a0ff4d5ab637306233da31f4db9130bc
Kirk Shillingford <tkshillinz@icloud.com>
2022-12-11 15:04:14 -0700

    2022-12-11 15:04:14 silliness

--- AOC2022/DayTen.fs
+++ AOC2022/DayTen.fs
@@ -206,7 +206,7 @@ let enumerate = Seq.collect parse >> Seq.scan (+) 1 >> Seq.indexed
 let part1: string seq -> int =
     enumerate
     >> Seq.map (tupleMap2 inc id)
-    >> Seq.filter (fun (idx, _) -> (idx % 40) - 20 = 0 && idx <= 220)
+    >> Seq.filter (fun (idx, _) -> (idx % 40) = 20 && idx <= 220)
     >> Seq.sumBy (fun (idx, v) -> idx * v)
 
 let pixelate =
