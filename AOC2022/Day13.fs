84046d7be0680ca958d47ce1d05280bc0b29719d
Kirk Shillingford <tkshillinz@icloud.com>
2022-12-22 12:59:28 -0700

    Update Day13.fs

--- AOC2022/Day13.fs
+++ AOC2022/Day13.fs
@@ -5,21 +5,17 @@ open FParsec.Pipes
 open FSharpx.Collections
 open Utility
 
-type Nested =
-    | N of int
-    | L of Nested list
+type Nested = N of int | L of Nested list
 
 let nested, nestedRef = createParserForwardedToRef<Nested, unit> ()
 
 let nParser: Parser<Nested, unit> = %% +.integerParser -|> N
 
-let lParser: Parser<Nested, unit> =
-    %% '[' -- +.(nested * (qty[0..] / ',')) -- ']' -|> (ResizeArray.toList >> L)
+let lParser: Parser<Nested, unit> = %% '[' -- +.(nested * (qty[0..] / ',')) -- ']' -|> (ResizeArray.toList >> L)
 
 do nestedRef := %[ nParser; lParser ]
 
-let rec plumb (L left) (L right) =
-    match (left, right) with
+let rec plumb (L left) (L right) = match (left, right) with
     | [], _ -> -1
     | _, [] -> 1
     | N num :: _, N num2 :: _ when num <> num2 -> compare num num2
@@ -31,21 +27,15 @@ let rec plumb (L left) (L right) =
 let parse = Seq.choose ((run lParser) >> parserResultToOption)
 
 let part1 =
-    parse
-    >> Seq.chunkBySize 2
-    >> indexWithOffset 1
+    parse >> Seq.chunkBySize 2 >> indexWithOffset 1
     >> Seq.filter (snd >> (fun pair -> pair = Array.sortWith plumb pair))
     >> Seq.sumBy fst
 
 let additions = parse [| "[[2]]"; "[[6]]" |]
 
 let part2 =
-    parse
-    >> Seq.append additions
-    >> Seq.sortWith plumb
-    >> indexWithOffset 1
+    parse >> Seq.append additions >> Seq.sortWith plumb >> indexWithOffset 1
     >> Seq.filter (snd >> (=) >> flip Seq.exists additions)
-    >> Seq.map fst
-    >> Seq.reduce (*)
+    >> Seq.map fst >> Seq.reduce (*)
 
 let solution = Solution.build (part1, part2)
